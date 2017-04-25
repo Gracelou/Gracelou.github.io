8bc04660761da9007ff9dafd92ddfa8cccad9390
gracelou <lb2105162660@gmail.com>
2017-04-25 21:35:49 +0800

    submit

---
+++ js/m_common.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+// 	var date = new Date();
+// 	var data = date.getDate()
+// 	var month = date.getMonth() 
+// 	var year = date.getFullYear()
+// 	var t = year +'-'+ month +'-'+ data
+// 	$(".time").text(t);
+// })
